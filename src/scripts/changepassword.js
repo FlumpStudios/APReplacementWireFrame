061494c2f805615123704ee74d619ad7c6e37d3a
Paul Marrable <paul.marrable@Marketing.ListersGroup.local>
2019-01-29 14:40:52 +0000

    added change password and help screens;ameneded index for better navigation

--- src/scripts/changepassword.js
+++ src/scripts/changepassword.js
@@ -1,3 +1,26 @@
-$("document").ready(() => {
-    console.log("Hello World");
-});
+$("document.html").ready(() => {
+    $("#submit-form").click(() => handleSubmit())
+    }        
+);
+
+function handleSubmit(){
+    var mediumRegex = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
+
+    let newpass = $("#new-password").val();
+    let confirmPass = $("#confirm-password").val();
+    if ($("#change-password-form").valid()){  
+        
+        if (mediumRegex.test(newpass)){
+         if(newpass !== confirmPass){
+            $("#response-message").text("Passwords do not match");     
+         }
+         else{   
+            $("#response-message").text("Password successfully changed");
+        }
+    }
+    else{
+        $("#response-message").text("Password does not meet the required strench criteria");
+    }
+    
+    }
+}
