8cc0986d5db9b41971113cdf0499f1f7f50cb028
Paul Marrable <paul.marrable@Marketing.ListersGroup.local>
2019-01-30 12:21:02 +0000

    added supplementary order page and expected json obj

---
+++ src/scripts/supplementaryOrder.js
@@ -0,0 +1,17 @@
+let clientData ={
+    salesPerson:"",
+    customerName: "",
+    vehicleReg: "",
+    policyNumber: 0,
+    makeAndModel: "",
+    registrationDate: "",
+    milage: 0,
+    purchaseDate: "",
+    PurchasePrice: 0,
+    policyDetails: [{
+        coverStartDate:0,
+        retailNettPrice:0,
+        vat:0,
+        retailPrice:0
+    }]
+}
