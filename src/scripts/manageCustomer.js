04d229a6680601f58ecb424be465fb7a70cfedf9
Paul Marrable <paul.marrable@Marketing.ListersGroup.local>
2019-01-30 14:48:33 +0000

    Added additional functionality to the Manage Customer view

--- src/scripts/manageCustomer.js
+++ src/scripts/manageCustomer.js
@@ -13,4 +13,18 @@ let customerDetail = {
 $(document).ready(function(){
     $("[data-toggle=popover]")
     .popover({html:true})
+
+    $("#customer-initiated-check").change(() => handleCustomerInitiatedChecked());
+    $(".confirm-submission").click( () => handleConfirm() )
 });
+
+function handleCustomerInitiatedChecked(){
+    $('#confirm-modal').modal('show')
+}
+
+function handleConfirm(){
+  $("#customer-initiated-check").prop("disabled","true");
+  $("#confirm-modal").modal("hide");
+  $("#question-icon").toggleClass('fa-question-circle fa-check-circle');
+  
+}
